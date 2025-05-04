ee7f1fef3bbdb46f88bfd1190b75d8f7758b37ec
pedrolazzaroni <phlazzaroni@gmail.com>
2025-05-04 12:03:31 -0300

    Finalizando footer

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="additional-styles.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+     <!-- Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 <body class="page-transition">
     <!-- Preloader -->
